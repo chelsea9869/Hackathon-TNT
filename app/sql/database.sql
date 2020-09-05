d0e1edb59e6c2e84f965ca974bab749643694ad7
Haodi-Qi <haodi.qi@gmail.com>
2020-09-05 21:49:23 +0800

    add database creation script

---
+++ app/sql/database.sql
@@ -0,0 +1,21 @@
+drop schema if exists hackathon;
+create schema hackathon;
+use hackathon;
+
+create table user(
+	username varchar(128) primary key,
+    profile varchar(5)
+);
+
+create table bookkeeping(
+	id int auto_increment primary key,
+	username varchar(128) NOT NULL,
+    date date NOT NULL,
+    item varchar(128) NOT NULL,
+    type varchar(7) NOT NULL,
+    category varchar(50) NOT NULL,
+    amount float NOT NULL,
+    constraint foreign key (username) references user(username) ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+Insert into user values ("amy", "high");
