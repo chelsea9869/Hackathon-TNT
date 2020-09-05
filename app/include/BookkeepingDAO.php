e6eeca9ebe069d3cef5b2a1c513c3c1a87ac8884
Haodi-Qi <haodi.qi@gmail.com>
2020-09-05 21:44:00 +0800

    add in BookkeepingDAO + connection manager

---
+++ app/include/BookkeepingDAO.php
@@ -0,0 +1,65 @@
+<?php
+require_once 'common.php';
+class BookkeepingDAO
+{
+    
+    public function retrieveByPersonByDate($username,$date){
+        
+        $conn_manager = new ConnectionManager();
+        $conn = $conn_manager->getConnection();
+        
+        $sql = 'SELECT * FROM Bookkeeping WHERE username=:username AND date=:date';
+        $stmt = $conn->prepare($sql);
+        @$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        @$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+        $stmt->setFetchMode(PDO::FETCH_ASSOC);
+        $stmt->execute();
+
+        $records = [];
+        while ($row = $stmt->fetch()) {
+            $record = [];
+            $record['date'] = $row['date'];
+            $record['item'] = $row['item'];
+            $record['type'] = $row['type'];
+            $record['category'] = $row['category'];
+            $record['amount'] = $row['amount'];
+            $records[] = $record;
+        }
+
+        $stmt->closeCursor();
+        $conn = null;
+
+        return $records;
+    }
+
+    /*
+    @param $username: str, unique identifier of the user
+    @param $item_info: associative array contains: item, type, category, amount
+    @return $status: bool
+    */
+    public function addRecord($username,$date, $item_info){
+        $conn_manager = new ConnectionManager();
+        $conn = $conn_manager->getConnection();
+        
+        $sql = 'INSERT INTO Bookkeeping (username,date, item, type, category, amount) VALUES (:username,:date, :item, :type, :category, :amount)';
+        $stmt = $conn->prepare($sql);
+        @$stmt->bindParam(':username', $username, PDO::PARAM_STR);
+        @$stmt->bindParam(':date', $date, PDO::PARAM_STR);
+        @$stmt->bindParam(':item', $item_info['item'], PDO::PARAM_STR);
+        @$stmt->bindParam(':type', $item_info['type'], PDO::PARAM_STR);
+        @$stmt->bindParam(':category', $item_info['category'], PDO::PARAM_STR);
+        @$stmt->bindParam(':amount', $item_info['amount'], PDO::PARAM_STR);
+        
+        if (!$status = $stmt->execute()) {
+            return $stmt->errorInfo()[2];
+        }
+
+        $stmt->closeCursor();
+        $conn = null;
+
+        return $status;
+    }
+
+}
+
+?>
