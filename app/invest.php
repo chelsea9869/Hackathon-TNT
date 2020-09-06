95823b2aa9a61c7661c1502a6f1663cc01bfc659
JeffreyZe <xuze.wang.2017@sis.smu.edu.sg>
2020-09-06 21:59:53 +0800

    Add Pricing page

--- app/invest.php
+++ app/invest.php
@@ -33,8 +33,57 @@
 
     <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
         <h1 class="display-4"></h1>
-        <p class="lead">Upgrad to unlock investment simulation</p>
+        <p class="lead">Upgrade to Unlock Investment Simulation</p>
     </div>
+
+    <div class="container">
+      <div class="card-deck mb-3 text-center">
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Free</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>Free Basic Plan</li>
+              <li>Bookkeeping</li>
+              <li>Scenario Games</li>
+              <li>Market Intelligence</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-outline-primary">Current Plan</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Pro</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$5 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>All features in Free Plan</li>
+              <li>Access to Investment Simulation</li>
+              <li>Investment Advice</li>
+              <li>Help Center Access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Subscribe</button>
+          </div>
+        </div>
+        <div class="card mb-4 box-shadow">
+          <div class="card-header">
+            <h4 class="my-0 font-weight-normal">Enterprise</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>All features in Pro Plan</li>
+              <li>Access to Investment Platform</li>
+              <li>Instant Market Intelligence</li>
+              <li>24hrs Help center access</li>
+            </ul>
+            <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+          </div>
+        </div>
+      </div>
 
    
 
