3e5063b9a38de778cead7762c9d3aab67500fbf5
JeffreyZe <xuze.wang.2017@sis.smu.edu.sg>
2020-09-06 15:15:21 +0800

    Add img files

--- app/include/ConnectionManager.php
+++ app/include/ConnectionManager.php
@@ -8,18 +8,17 @@ class ConnectionManager {
         $username = "root";
         $dbname = "hackathon";
         $port = 3306;
-        $password = ""; 
-        if(strtoupper(substr(PHP_OS, 0, 3)) === 'LIN'){
-            $password='hackathon';
-        }elseif(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN'){
-	        $password = "root";
+	    $password = "ykJ7OaMCEexF"; 
+        if(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN'){
+            $password = "root";
+            $port = 8889;
 	    }
 
         $url  = "mysql:host={$host};dbname={$dbname};port={$port}";
         $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
         $conn = new PDO($url, $username, $password, $options);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
-        return $conn;  
+        return $conn;
         
     }
     
