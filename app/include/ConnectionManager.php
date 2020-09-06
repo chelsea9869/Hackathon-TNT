604c3d6e0fe7aec09e4bf58b1b58e670db040c7e
JeffreyZe <xuze.wang.2017@sis.smu.edu.sg>
2020-09-06 13:03:48 +0800

    Modify ConnectionManager

--- app/include/ConnectionManager.php
+++ app/include/ConnectionManager.php
@@ -3,18 +3,19 @@
 class ConnectionManager {
    
     public function getConnection() {
-	    $password = "";  
-        if(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN'){
-	        $password = 'ykJ7OaMCEexF';
-	    }
+
         $host = "localhost";
         $username = "root";
         $dbname = "hackathon";
-        $port = 3306;    
+        $port = 3306;
+	    $password = "ykJ7OaMCEexF"; 
+        if(strtoupper(substr(PHP_OS, 0, 3)) !== 'WIN'){
+	        $password = "root";
+	    }
 
         $url  = "mysql:host={$host};dbname={$dbname};port={$port}";
         $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',);
-        $conn = new PDO($url, $username, $password,$options);
+        $conn = new PDO($url, $username, $password, $options);
         $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
         return $conn;  
         
